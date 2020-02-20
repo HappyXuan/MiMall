29335a2cde9bbd2e72eb367a797aef5b2a574bb4
HappyXuan <linxuan1103@gmail.com>
2020-02-20 21:01:38 +0800

    项目目录结构分支

---
+++ src/pages/home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <nav-header></nav-header>
+        <router-view></router-view>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+
+<script>
+import NavHeaderVue from '../components/NavHeader.vue'
+import NavFooterVue from '../components/NavFooter.vue'
+export default {
+    name:'home',
+    components:[NavHeaderVue, NavFooterVue]
+}
+</script>
